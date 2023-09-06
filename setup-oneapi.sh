2d3c4eb6b5200482474f59c72210ebce1e53921e
a-One-Fan <espamholding@gmail.com>
2023-09-07 01:50:54 +0300

    Speed up setup Download intel's 500mb wheels, use mega only for 3mb torchaudio

--- setup-oneapi.sh
+++ setup-oneapi.sh
@@ -19,17 +19,15 @@ sudo apt install libportaudio2 libasound-dev megatools
 
 python3.10 -m venv rvcvenv
 source ./rvcvenv/bin/activate
+pip install --upgrade pip
 pip install astunparse numpy==1.25.2 pyyaml pytest psutil setuptools cffi typing_extensions future six requests hypothesis expecttest types-dataclasses dataclasses Pillow SoundFIle==0.12.1 kaldi-io==0.9.8 scipy==1.11.2
 mkdir megatemp
 cd ./megatemp
-megadl https://mega.nz/file/ndIRTYzQ#tn8teb5dXZQ6GNJBfRxykI1zQ58f-xBu8wNi-usYYho
-tar -xzvf ./wheels.tar.gz
-python -m pip install torch-2.0.1a0+gite9ebda2-cp310-cp310-linux_x86_64.whl
-python -m pip install --no-deps torchvision-0.15.2a0+fa99a53-cp310-cp310-linux_x86_64.whl
-python -m pip install --no-deps torchaudio-2.0.2+31de77d-cp310-cp310-linux_x86_64.whl
-python -m pip install intel_extension_for_pytorch-2.0.110+gite29c5fb-cp310-cp310-linux_x86_64.whl
+megadl https://mega.nz/file/bR4CzDJB#J0J5ZAQCcqAyOVHq_O2ivp0fKZ3hAiXjQ8VdG76hv9c
+pip install --no-deps torchaudio-2.0.2+31de77d-cp310-cp310-linux_x86_64.whl
 cd ..
 rm -rf ./megatemp
+pip install torch==2.0.1a0 torchvision==0.15.2a0 intel_extension_for_pytorch==2.0.110+xpu -f https://developer.intel.com/ipex-whl-stable-xpu
 cd ./server
 pip install -r requirements.txt
 cd ..
